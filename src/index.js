bf7b0d5438c0de47f4c090331f26f9e46ad94e0e
ys9412 <60271068+ys9412@users.noreply.github.com>
2020-04-09 18:21:14 -0700

    notification icons are added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
+window.$proxyurl = "https://cors-anywhere.herokuapp.com/";
+window.$api = "http://ec2-52-91-80-144.compute-1.amazonaws.com/api/getbyid=1";
 
 ReactDOM.render(
   <Router>
