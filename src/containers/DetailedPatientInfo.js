138b990051bbfb49e6f26b255af5f0c256047ed0
Harrison Quarry <harrisonqleddy@gmail.com>
2020-06-09 08:16:35 -0700

    Set y-axis scale from 0 - 180 bpm

--- src/containers/DetailedPatientInfo.js
+++ src/containers/DetailedPatientInfo.js
@@ -295,6 +295,14 @@ class DetailedPatientInfo extends Component {
               display: true,
               position: "bottom",
             },
+            scales: {
+              yAxes: [{
+                ticks: {
+                  min: 0,
+                  max: 180,
+                }
+              }]
+            },
           }}
         />
         <p className="break"></p>
